affa8b6a927e3b822150fd763fa68431e36968b5
aZaheer008 <kmitinstitute008@gmail.com>
2021-08-27 23:21:16 +0500

    intialize app component testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React , { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
- function App () {
+ class App extends Component {
+   render() {
     return (
-      <div>
+      <div data-test="component-app">
         
       </div>
     );
+   }
 };
 
 export default App
